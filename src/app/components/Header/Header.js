e7d72df2b7310f247c1e786165bb0987960be6d5
Ajeeth <Ajeeth@s7works.io>
2024-06-18 11:19:12 +0530

    Mobile View Updated

--- src/app/components/Header/Header.js
+++ src/app/components/Header/Header.js
@@ -1,9 +1,9 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import Image from "next/image";
-import { IoMenu } from "react-icons/io5";
-import "./header.css";
+import { useState } from 'react';
+import Image from 'next/image';
+import { IoMenu } from 'react-icons/io5';
+import './header.css';
 
 const Header = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -12,52 +12,65 @@ const Header = () => {
     setMobileMenuOpen(!mobileMenuOpen);
   };
 
+  const handleNavLinkClick = () => {
+    if (mobileMenuOpen) {
+      toggleMobileMenu();
+    }
+  };
+
   return (
-    <header className="header">
-      <div className="logoContainer">
-        <a href="#">
+    <header className='header'>
+      <div className='logoContainer'>
+        <a href='#'>
           <Image
-            src="/TATAMOTORS.png"
+            src='/TATAMOTORS.png'
             width={250}
             height={250}
-            alt=""
-            className="companyLogo tataLogo"
+            alt=''
+            className='companyLogo tataLogo'
           />
         </a>
       </div>
-      <nav className={mobileMenuOpen ? "navActive" : ""}>
-        <ul className="navList" onClick={toggleMobileMenu}>
+      <nav className={mobileMenuOpen ? 'navActive' : ''}>
+        <ul className='navList'>
           <li>
-            <a href="#about">About</a>
+            <a href='#about' onClick={handleNavLinkClick}>
+              About
+            </a>
           </li>
           <li>
-            <a href="#services">Services</a>
+            <a href='#services' onClick={handleNavLinkClick}>
+              Services
+            </a>
           </li>
           <li>
-            <a href="#whyUs">Why Us</a>
+            <a href='#whyUs' onClick={handleNavLinkClick}>
+              Why Us
+            </a>
           </li>
           <li>
-            <a href="#outlets">Outlets</a>
+            <a href='#outlets' onClick={handleNavLinkClick}>
+              Outlets
+            </a>
           </li>
           <li>
-            <a href="#outlets">
-              <button className="buttonInverse">Make an Appointment</button>
+            <a href='#outlets' onClick={handleNavLinkClick}>
+              <button className='buttonInverse'>Make an Appointment</button>
             </a>
           </li>
         </ul>
       </nav>
-      <div className="mobileMenuToggle" onClick={toggleMobileMenu}>
+      <div className='mobileMenuToggle' onClick={toggleMobileMenu}>
         <IoMenu />
       </div>
-      <div className="logoContainer">
-        <a href="#">
-          {" "}
+      <div className='logoContainer'>
+        <a href='#'>
           <Image
-            src="/AlagappaEngineering.png"
+            src='/AlagappaEngineering.png'
             width={250}
             height={250}
-            alt=""
-            className="companyLogo"
+            alt=''
+            className='companyLogo'
           />
         </a>
       </div>
